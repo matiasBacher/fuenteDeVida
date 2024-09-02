f64fcd1bf985174d2594ccc16203586e4578dc42
Matias Bacher <matiasadrianbacher.2006@gmail.com>
2024-09-02 18:06:59 -0300

    primer comentario

---
+++ app/vista/caja.php
@@ -0,0 +1,4 @@
+<!-- crear una interfaz similar al prototipo caja 
+ https://matias-bacher.atlassian.net/wiki/spaces/FDV/pages/16876149/Prototipo
+para ver la pagina entrar a localhost/index.php?p=caja
+  -->
