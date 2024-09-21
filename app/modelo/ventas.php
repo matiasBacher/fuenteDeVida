0f7eab339e579ad9fe78e77793281ac63c7e1f03
Matias Bacher <matiasadrianbacher.2006@gmail.com>
2024-09-20 22:44:55 -0300

    doctrine

---
+++ app/modelo/ventas.php
@@ -0,0 +1,174 @@
+<?php 
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/vendor/autoload.php");
+use doctrine\ORM\Mapping as ORM;
+
+
+
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+#[ORM\Entity]
+#[ORM\Table(name: "ventas")]
+class Venta
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: "IDENTITY")]
+    #[ORM\Column(name: "ID_VENTA", type: "integer")]
+    private ?int $id = null;
+
+    #[ORM\Column(name: "NUMEROFACTURAVENTAS", type: "integer", nullable: true)]
+    private ?int $numeroFactura = null;
+
+    #[ORM\Column(name: "FECHA_VENTA", type: "date", options: ["default" => "CURRENT_TIMESTAMP"])]
+    private \DateTime $fecha;
+
+    #[ORM\Column(name: "HORA_VENTA", type: "time", options: ["default" => "CURRENT_TIMESTAMP"])]
+    private \DateTime $hora;
+
+    #[ORM\Column(name: "METODODEPAGO_VENTA", type: "string", length: 250)]
+    private string $metodoPago;
+
+    #[ORM\Column(name: "ERROR_VENTA", type: "boolean")]
+    private bool $errorVenta;
+
+    #[ORM\Column(name: "motivoCorreccion", type: "string", length: 250, nullable: true)]
+    private ?string $motivoCorreccion = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class)]
+    #[ORM\JoinColumn(name: "ventaAnterior", referencedColumnName: "ID_VENTA", nullable: true, onDelete: "SET NULL")]
+    private ?self $ventaAnterior = null;
+
+    #[ORM\OneToMany(mappedBy: "venta", targetEntity: DetalleDeVenta::class, cascade: ["persist", "remove"])]
+    private Collection $detalles;
+
+    // Constructor
+    public function __construct(?array $detalles = null)
+    {
+        $this->fecha = new \DateTime();  // Inicializa con la fecha actual
+        $this->hora = new \DateTime();   // Inicializa con la hora actual
+        $this->errorVenta = false;
+        $this->metodoPago = 'Efectivo';
+        $this->detalles = new ArrayCollection();  // Inicializa la colección de detalles
+
+        // Si se pasan detalles en el constructor, los añadimos a la colección
+        if ($detalles) {
+            foreach ($detalles as $detalle) {
+                $this->addDetalle($detalle);
+            }
+        }
+    }
+
+    // Métodos para manejar la relación con DetalleDeVenta
+
+    public function addDetalle(DetalleDeVenta $detalle): self
+    {
+        if (!$this->detalles->contains($detalle)) {
+            $this->detalles->add($detalle);
+            $detalle->setVenta($this);
+        }
+        return $this;
+    }
+
+    public function removeDetalle(DetalleDeVenta $detalle): self
+    {
+        if ($this->detalles->contains($detalle)) {
+            $this->detalles->removeElement($detalle);
+            if ($detalle->getVenta() === $this) {
+                $detalle->setVenta(null);
+            }
+        }
+        return $this;
+    }
+
+    public function getDetalles(): Collection
+    {
+        return $this->detalles;
+    }
+
+    // Otros getters y setters...
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumeroFactura(): ?int
+    {
+        return $this->numeroFactura;
+    }
+
+    public function setNumeroFactura(?int $numeroFactura): self
+    {
+        $this->numeroFactura = $numeroFactura;
+        return $this;
+    }
+
+    public function getFecha(): \DateTime
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTime $fecha): self
+    {
+        $this->fecha = $fecha;
+        return $this;
+    }
+
+    public function getHora(): \DateTime
+    {
+        return $this->hora;
+    }
+
+    public function setHora(\DateTime $hora): self
+    {
+        $this->hora = $hora;
+        return $this;
+    }
+
+    public function getMetodoPago(): string
+    {
+        return $this->metodoPago;
+    }
+
+    public function setMetodoPago(string $metodoPago): self
+    {
+        $this->metodoPago = $metodoPago;
+        return $this;
+    }
+
+    public function getErrorVenta(): bool
+    {
+        return $this->errorVenta;
+    }
+
+    public function setErrorVenta(bool $errorVenta): self
+    {
+        $this->errorVenta = $errorVenta;
+        return $this;
+    }
+
+    public function getMotivoCorreccion(): ?string
+    {
+        return $this->motivoCorreccion;
+    }
+
+    public function setMotivoCorreccion(?string $motivoCorreccion): self
+    {
+        $this->motivoCorreccion = $motivoCorreccion;
+        return $this;
+    }
+
+    public function getVentaAnterior(): ?self
+    {
+        return $this->ventaAnterior;
+    }
+
+    public function setVentaAnterior(?self $ventaAnterior): self
+    {
+        $this->ventaAnterior = $ventaAnterior;
+        return $this;
+    }
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,11 +6,41 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitac2d307f4ed514763a8119f3523066bb
 {
+    public static $files = array (
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
+        'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
+        '0d59ee240a4cd96ddbb4ff164fccea4d' => __DIR__ . '/..' . '/symfony/polyfill-php73/bootstrap.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Php73\\' => 23,
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Polyfill\\Intl\\Normalizer\\' => 33,
+            'Symfony\\Polyfill\\Intl\\Grapheme\\' => 31,
+            'Symfony\\Polyfill\\Ctype\\' => 23,
+            'Symfony\\Contracts\\Service\\' => 26,
+            'Symfony\\Contracts\\Cache\\' => 24,
+            'Symfony\\Component\\Yaml\\' => 23,
+            'Symfony\\Component\\VarExporter\\' => 30,
+            'Symfony\\Component\\String\\' => 25,
+            'Symfony\\Component\\Console\\' => 26,
+            'Symfony\\Component\\Cache\\' => 24,
             'Svg\\' => 4,
             'Sabberworm\\CSS\\' => 15,
+        ),
+        'P' => 
+        array (
+            'Psr\\Log\\' => 8,
+            'Psr\\Container\\' => 14,
+            'Psr\\Cache\\' => 10,
         ),
         'M' => 
         array (
@@ -23,10 +53,72 @@ class ComposerStaticInitac2d307f4ed514763a8119f3523066bb
         'D' => 
         array (
             'Dompdf\\' => 7,
+            'Doctrine\\Persistence\\' => 21,
+            'Doctrine\\ORM\\' => 13,
+            'Doctrine\\Instantiator\\' => 22,
+            'Doctrine\\Inflector\\' => 19,
+            'Doctrine\\Deprecations\\' => 22,
+            'Doctrine\\DBAL\\' => 14,
+            'Doctrine\\Common\\Lexer\\' => 22,
+            'Doctrine\\Common\\Collections\\' => 28,
+            'Doctrine\\Common\\Cache\\' => 22,
+            'Doctrine\\Common\\' => 16,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'Symfony\\Polyfill\\Php80\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
+        ),
+        'Symfony\\Polyfill\\Php73\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php73',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Polyfill\\Intl\\Normalizer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer',
+        ),
+        'Symfony\\Polyfill\\Intl\\Grapheme\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme',
+        ),
+        'Symfony\\Polyfill\\Ctype\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'Symfony\\Contracts\\Service\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/service-contracts',
+        ),
+        'Symfony\\Contracts\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/cache-contracts',
+        ),
+        'Symfony\\Component\\Yaml\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/yaml',
+        ),
+        'Symfony\\Component\\VarExporter\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/var-exporter',
+        ),
+        'Symfony\\Component\\String\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/string',
+        ),
+        'Symfony\\Component\\Console\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/console',
+        ),
+        'Symfony\\Component\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/cache',
+        ),
         'Svg\\' => 
         array (
             0 => __DIR__ . '/..' . '/dompdf/php-svg-lib/src/Svg',
@@ -34,6 +126,18 @@ class ComposerStaticInitac2d307f4ed514763a8119f3523066bb
         'Sabberworm\\CSS\\' => 
         array (
             0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/src',
+        ),
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/src',
+        ),
+        'Psr\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
+        'Psr\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/cache/src',
         ),
         'Masterminds\\' => 
         array (
@@ -47,11 +151,63 @@ class ComposerStaticInitac2d307f4ed514763a8119f3523066bb
         array (
             0 => __DIR__ . '/..' . '/dompdf/dompdf/src',
         ),
+        'Doctrine\\Persistence\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/persistence/src/Persistence',
+        ),
+        'Doctrine\\ORM\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/orm/src',
+        ),
+        'Doctrine\\Instantiator\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/instantiator/src/Doctrine/Instantiator',
+        ),
+        'Doctrine\\Inflector\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/inflector/lib/Doctrine/Inflector',
+        ),
+        'Doctrine\\Deprecations\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/deprecations/lib/Doctrine/Deprecations',
+        ),
+        'Doctrine\\DBAL\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/dbal/src',
+        ),
+        'Doctrine\\Common\\Lexer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/lexer/src',
+        ),
+        'Doctrine\\Common\\Collections\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/collections/lib/Doctrine/Common/Collections',
+        ),
+        'Doctrine\\Common\\Cache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/cache/lib/Doctrine/Common/Cache',
+        ),
+        'Doctrine\\Common\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/common/src',
+            1 => __DIR__ . '/..' . '/doctrine/event-manager/src',
+        ),
+    );
+
+    public static $fallbackDirsPsr0 = array (
+        0 => __DIR__ . '/../..' . '/app',
     );
 
     public static $classMap = array (
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Dompdf\\Cpdf' => __DIR__ . '/..' . '/dompdf/dompdf/lib/Cpdf.php',
+        'JsonException' => __DIR__ . '/..' . '/symfony/polyfill-php73/Resources/stubs/JsonException.php',
+        'Normalizer' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php',
+        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -59,6 +215,7 @@ class ComposerStaticInitac2d307f4ed514763a8119f3523066bb
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitac2d307f4ed514763a8119f3523066bb::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitac2d307f4ed514763a8119f3523066bb::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr0 = ComposerStaticInitac2d307f4ed514763a8119f3523066bb::$fallbackDirsPsr0;
             $loader->classMap = ComposerStaticInitac2d307f4ed514763a8119f3523066bb::$classMap;
 
         }, null, ClassLoader::class);
