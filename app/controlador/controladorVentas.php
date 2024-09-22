6331b32952f3734cc3dd7c1383793fc8e681be06
Matias Bacher <matiasadrianbacher.2006@gmail.com>
2024-09-21 23:47:06 -0300

    fin del sabado, agregar opciones al controlador de ventas

--- app/controlador/controladorVentas.php
+++ app/controlador/controladorVentas.php
@@ -41,4 +41,59 @@ if(isset ($_POST["accion"])){
             ]);
         }
     }
+    if($_POST["accion"]=="registrarModificacionVenta"){
+        $ventaAnterior=$entityManager->find(Venta::class,$_Post["idVentaModificar"]);
+        $ventaAnterior->setErrorVenta(true);         
 
+        $detalleVentaJson = json_decode($_POST["detalleVenta"]);
+        $detallesventa = []; 
+        foreach($detalleVentaJson as $detalle){
+
+            $detallesventa[]=new DetalleVenta( $detalle->cantidad, $detalle->id) ;
+        }
+
+        $venta= new Venta($_POST["metodo"], $detallesventa);
+        $venta->setVentaAnterior($ventaAnterior);
+        $venta->setMotivoCorreccion($_POST["motivoCorreccionVenta"]);
+        try{
+
+            $entityManager->persist($venta);
+            $entityManager->persist($ventaAnterior);
+
+        }
+        catch(Exception $e) {
+            return json_encode([
+                "mensaje"=>-2
+            ]);    
+        }
+        try{
+            $entityManager->flush();
+        }
+        catch(Exception $e){
+            return json_encode(
+                [
+                    "mensaje"=>-1
+                ]
+                );
+        } 
+            return json_encode([
+                "mensaje"=>1
+            ]);
+        }
+    if($_POST["accion"]=="consultarVentas"){
+        $ventas=$entityManager->getRepository(Venta::class)->findBy([
+            "errorVenta"=>false,
+        ]);
+        return json_encode($ventas); 
+    }
+    if($_POST["accion"]=="consultarVentasCorregidas"){
+        $ultimaVenta=$entityManager->find(Venta::class, $_POST["ultimo"]);
+        $ventasAnteriores = [];
+        $puntero=$ultimaVenta;
+        while($puntero!==null){
+            $ventasAnteriores[]=$puntero;
+            $puntero=$puntero->getVentaAnterior();
+
+        }
+        return json_encode($ventasAnteriores);
+    }
